66b9d3879251b184d98ad140a827988699a93771
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-01-20 23:10:48 +0300

    calling two simple APIs

--- src/App.js
+++ src/App.js
@@ -4,36 +4,67 @@ import axios from "axios";
 
 function App() {
   const [posts, setPosts] = useState(null);
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [postsError, setPostsError] = useState("");
+  const [postsLoading, setPostsLoading] = useState(false);
+
+  const [comments, setComments] = useState(null);
+  const [commentsError, setCommmentsError] = useState("");
+  const [commentsLoading, setCommentsLoading] = useState(false);
 
   useEffect(() => {
     handlePosts();
+    handleComments();
   }, []);
 
   const handlePosts = async () => {
-    setLoading(true);
+    setPostsLoading(true);
     try {
       const result = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
       setPosts(result.data);
     } catch (err) {
-      setError(err.message || "Unexpected Error!");
+      setPostsError(err.message || "Unexpected Error!");
     } finally {
-      setLoading(false);
+      setPostsLoading(false);
+    }
+  };
+
+  const handleComments = async () => {
+    setCommentsLoading(true);
+    try {
+      const result = await axios.get(
+        "https://jsonplaceholder.typicode.com/comments"
+      );
+      setComments(result.data);
+    } catch (err) {
+      setCommmentsError(err.message || "Unexpected Error!");
+    } finally {
+      setCommentsLoading(false);
     }
   };
 
   return (
     <div className="App">
+      {/* Post List */}
       <div>
         <h1>Posts</h1>
-        {loading && <p>Posts are loading!</p>}
-        {error && <p>{error}</p>}
+        {postsLoading && <p>Posts are loading!</p>}
+        {postsError && <p>{postsError}</p>}
         <ul>
           {posts?.map((post) => (
             <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+      {/* Comment List */}
+      <div>
+        <h1>Comments</h1>
+        {commentsLoading && <p>Comments are loading!</p>}
+        {commentsError && <p>{commentsError}</p>}
+        <ul>
+          {comments?.map((comment) => (
+            <li key={comment.id}>{comment.name}</li>
           ))}
         </ul>
       </div>
